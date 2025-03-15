5f7efe8e922ed6aad98dc77a128c5b4b0487195b
Isaac Boots <isaacboots1@gmail.com>
2025-03-15 03:52:18 -0700

    added completions.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,26 +1,37 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "pyright", "ruff"},
-			})
-		end,
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.ruff.setup({})
+			lspconfig.lua_ls.setup({
+				capabilites = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilites = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilites = capabilities,
+			})
+			lspconfig.ruff.setup({
+				capabilites = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,5 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
