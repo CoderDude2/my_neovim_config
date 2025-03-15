eadb2dbe538db03539e53a11051d7c84d0c9c35f
Isaac Boots <isaacboots1@gmail.com>
2025-03-15 11:50:56 -0700

    Updated vim-options to not yank to clipboard

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,6 +32,9 @@ return {
 			lspconfig.ruff.setup({
 				capabilites = capabilities,
 			})
+            lspconfig.clangd.setup({
+				capabilites = capabilities,
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
